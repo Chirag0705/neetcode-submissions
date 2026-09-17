5ed5c33e13b5b7f3c6d572971b97f8e14778fdd4
Chirag M <156502427+Chirag0705@users.noreply.github.com>
2026-09-17 08:59:48 +0530

    Add: subsets-ii - submission-0

---
+++ Data Structures & Algorithms/subsets-ii/submission-0.cpp
@@ -0,0 +1,31 @@
+class Solution {
+    void findsun(int ind, vector<int>& nums, vector<int>& ds,
+                 vector<vector<int>>& ans)
+    {
+        ans.push_back(ds);
+
+        for(int i = ind; i < nums.size(); i++)
+        {
+            if(i > ind && nums[i] == nums[i-1])
+                continue;
+
+            ds.push_back(nums[i]);
+
+            findsun(i+1, nums, ds, ans);
+
+            ds.pop_back();
+        }
+    }
+
+public:
+    vector<vector<int>> subsetsWithDup(vector<int>& nums) {
+        sort(nums.begin(), nums.end());
+
+        vector<vector<int>> ans;
+        vector<int> ds;
+
+        findsun(0, nums, ds, ans);
+
+        return ans;
+    }
+};
